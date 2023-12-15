ef8e4a8276f5e12209e473c934d1bf006b70ed24
remco van der Velde <remco.pc@outlook.com>
2023-12-15 23:12:05 +0100

    system event action - event.action

--- src/Package/Trait/Import.php
+++ src/Package/Trait/Import.php
@@ -11,7 +11,8 @@ use R3m\Io\Node\Model\Node;
 use Exception;
 trait Import {
 
-    public function role_system(){
+    public function role_system(): void
+    {
         $object = $this->object();
         $node = new Node($object);
         $node->role_system_create('r3m_io/event');
